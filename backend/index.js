dd16c1a282af827c9dedcbc4db6e030d4b5fcb94
RushikeshT14 <rushitodkari0108@gmail.com>
2025-11-12 23:44:27 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+
+require("dotenv").config();
+
+const express = require("express");
+const mongoose =require("mongoose");
+
+const PORT = process.env.PORT || 3002;
+const URL = process.env.MONGO_URL;
+
+const app = express();
+
+
+app.listen(PORT, () => {
+  console.log("app sarted");
+  mongoose.connect(URL);
+  console.log("DB Connected");
+});
+
